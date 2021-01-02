e81690334f0af683a168315bbde760c7e7c16e3e
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2021-01-01 22:21:01 -0600

    added initial render function

--- src/index.js
+++ src/index.js
@@ -10,15 +10,18 @@ button.onclick = () => {
     render();
 }
 
-function render(toggle) {
+function render() {
     clearParent(getMainContainer())
     const newDice = Dice(getRand(1,6))
     appendToParent(getMainContainer(),newDice.DOM)
-
-    
 }
 
-function  getRand(min,max) {
+function getRand(min,max) {
     return Math.floor(Math.random()*max + min)
 }
+
+(function initialRender()  {
+    const newDice = Dice(1);
+    appendToParent(getMainContainer(),newDice.DOM)
+})()
 
