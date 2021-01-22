3f1ff51774033d40900cd0460df5ca37331623fe
Juan Pablo Carvajal <jcarvajalabrahams@gmail.com>
2021-01-21 19:55:14 -0600

    gh-pages packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import {getMainContainer,getCounterDom,changeMargin,containerWidth} from './javascripts/dom'
-import {clickTossHandler,setNumberOfDicesHandler} from './javascripts/handlers'
+import {getCounterDom,changeMargin,containerWidth} from './javascripts/dom'
 import {getRand,getTotal} from './javascripts/math'
 import {Game} from './javascripts/gameObjects'
 import './styles/style.css'
 
-const container = getMainContainer();
-const select = document.querySelector("#dices")
 
-container.onclick = clickTossHandler;
-select.onchange = setNumberOfDicesHandler;
 
 const game = Game();
 
